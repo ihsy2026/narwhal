80551a8e6b4e459916775b199c115b9ce85c3b49
koco <1007836886@qq.com>
2026-01-22 15:40:47 +0800

    Revert "init: 添加"
    This reverts commit 62cd84b8c9712f2c21942df2351ada460bc33705.

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -21,7 +21,6 @@ struct State {
     last_committed_round: Round,
     // Keeps the last committed round for each authority. This map is used to clean up the dag and
     // ensure we don't commit twice the same certificate.
-    // 每个节点最新提交轮次，用于GC清理
     last_committed: HashMap<PublicKey, Round>,
     /// Keeps the latest committed certificate (and its parents) for every authority. Anything older
     /// must be regularly cleaned up through the function `update`.
@@ -30,36 +29,28 @@ struct State {
 
 impl State {
     fn new(genesis: Vec<Certificate>) -> Self {
-        // 用genesis初始化State
         let genesis = genesis
-            // 变为迭代器
             .into_iter()
-            // 对于每个证书x构造二元组，(PublicKey, (Digest, Certificate))
             .map(|x| (x.origin(), (x.digest(), x)))
-            // 收集成一个 HashMap
             .collect::<HashMap<_, _>>();
 
         Self {
             last_committed_round: 0,
-            // 从genesis中取出（PublicKey, Round）
             last_committed: genesis.iter().map(|(x, (_, y))| (*x, y.round())).collect(),
             dag: [(0, genesis)].iter().cloned().collect(),
         }
     }
 
     /// Update and clean up internal state base on committed certificates.
-    /// &mut self显式拿到可变引用，允许修改，如果是&self为不可变引用
-    /// 根据传进来的certificate更新state
     fn update(&mut self, certificate: &Certificate, gc_depth: Round) {
         self.last_committed
-            // 有则覆盖，空则跳过
             .entry(certificate.origin())
             .and_modify(|r| *r = max(*r, certificate.round()))
             .or_insert_with(|| certificate.round());
 
         let last_committed_round = *self.last_committed.values().max().unwrap();
         self.last_committed_round = last_committed_round;
-        // 删除已经提交的round-gc_depth之前的证书
+
         for (name, round) in &self.last_committed {
             self.dag.retain(|r, authorities| {
                 authorities.retain(|n, _| n != name || r >= round);
@@ -88,7 +79,6 @@ pub struct Consensus {
 }
 
 impl Consensus {
-    // 后台异步任务，捕获变量并调用共识主循环
     pub fn spawn(
         committee: Committee,
         gc_depth: Round,
@@ -110,21 +100,16 @@ impl Consensus {
         });
     }
 
-    //
     async fn run(&mut self) {
         // The consensus state (everything else is immutable).
-        // 初始化State状态
         let mut state = State::new(self.genesis.clone());
 
         // Listen to incoming certificates.
-        // 监听来自Primary的证书
         while let Some(certificate) = self.rx_primary.recv().await {
             debug!("Processing {:?}", certificate);
-            // 取出证书所在的轮次
             let round = certificate.round();
 
             // Add the new certificate to the local storage.
-            // 将新监听到的证书加入DAG，持续构建本地DAG视图
             state
                 .dag
                 .entry(round)
@@ -136,7 +121,6 @@ impl Consensus {
             let r = round - 1;
 
             // We only elect leaders for even round numbers.
-            // 偶数轮选择锚点
             if r % 2 != 0 || r < 4 {
                 continue;
             }
@@ -147,7 +131,6 @@ impl Consensus {
             if leader_round <= state.last_committed_round {
                 continue;
             }
-            // 取出锚点的证书，如果还没收到锚点则继续监听
             let (leader_digest, leader) = match self.leader(leader_round, &state.dag) {
                 Some(x) => x,
                 None => continue,
@@ -156,12 +139,12 @@ impl Consensus {
             // Check if the leader has f+1 support from its children (ie. round r-1).
             let stake: Stake = state
                 .dag
-                .get(&(r - 1)) // 取出r-1轮所有证书
-                .expect("We should have the whole history by now") // 没有找到历史就直接panic
-                .values() // 遍历这一轮所有的(Digest, Certificate)
-                .filter(|(_, x)| x.header.parents.contains(&leader_digest)) // 筛选出证书的parents包含leader的
-                .map(|(_, x)| self.committee.stake(&x.origin())) // 把支持者的stake取出来
-                .sum(); // 加和
+                .get(&(r - 1))
+                .expect("We should have the whole history by now")
+                .values()
+                .filter(|(_, x)| x.header.parents.contains(&leader_digest))
+                .map(|(_, x)| self.committee.stake(&x.origin()))
+                .sum();
 
             // If it is the case, we can commit the leader. But first, we need to recursively go back to
             // the last committed leader, and commit all preceding leaders in the right order. Committing
@@ -172,24 +155,20 @@ impl Consensus {
             }
 
             // Get an ordered list of past leaders that are linked to the current leader.
-            // leader 达标，准备生成提交序列 sequence
             debug!("Leader {:?} has enough support", leader);
             let mut sequence = Vec::new();
-            // 找出与当前leader往前连接的最前面的leader并反转，保证提交顺序正确
             for leader in self.order_leaders(leader, &state).iter().rev() {
                 // Starting from the oldest leader, flatten the sub-dag referenced by the leader.
                 for x in self.order_dag(leader, &state) {
                     // Update and clean up internal state.
-                    // 每提交一个证书更新state并进行GC
                     state.update(&x, self.gc_depth);
+
                     // Add the certificate to the sequence.
-                    // 把该证书加入最终输出序列
                     sequence.push(x);
                 }
             }
 
             // Log the latest committed round of every authority (for debug).
-            // 打印每个节点最新提交轮次
             if log_enabled!(log::Level::Debug) {
                 for (name, round) in &state.last_committed {
                     debug!("Latest commit of {}: Round {}", name, round);
@@ -197,7 +176,6 @@ impl Consensus {
             }
 
             // Output the sequence in the right order.
-            // 输出提交序列到 primary 和应用层
             for certificate in sequence {
                 #[cfg(not(feature = "benchmark"))]
                 info!("Committed {}", certificate.header);
@@ -241,24 +219,20 @@ impl Consensus {
     }
 
     /// Order the past leaders that we didn't already commit.
-    /// 把过去那些还没提交的 leader 按顺序挑出来
     fn order_leaders(&self, leader: &Certificate, state: &State) -> Vec<Certificate> {
         let mut to_commit = vec![leader.clone()];
         let mut leader = leader;
-        // 左闭右开区间，从state.last_committed_round + 2遍历到当前轮次，并且只遍历偶数轮
         for r in (state.last_committed_round + 2..leader.round())
             .rev()
             .step_by(2)
         {
             // Get the certificate proposed by the previous leader.
-            // 取出当前遍历轮次leadder的公钥与证书
             let (_, prev_leader) = match self.leader(r, &state.dag) {
                 Some(x) => x,
                 None => continue,
             };
 
             // Check whether there is a path between the last two leaders.
-            // 判断从当前 leader 出发，沿 DAG 的父引用往回追，能不能到达 prev_leader
             if self.linked(leader, prev_leader, &state.dag) {
                 to_commit.push(prev_leader.clone());
                 leader = prev_leader;
@@ -270,13 +244,12 @@ impl Consensus {
     /// Checks if there is a path between two leaders.
     fn linked(&self, leader: &Certificate, prev_leader: &Certificate, dag: &Dag) -> bool {
         let mut parents = vec![leader];
-        // 从leader_round-1开始向前遍历
         for r in (prev_leader.round()..leader.round()).rev() {
             parents = dag
-                .get(&(r)) // 取出类型为HashMap<PublicKey, (Digest, Certificate)>
-                .expect("We should have the whole history by now") // 某一轮缺失直接panic
-                .values() // 遍历这一轮所有的(Digest, Certificate)
-                .filter(|(digest, _)| parents.iter().any(|x| x.header.parents.contains(digest))) // 被parents集合里的节点引用的集合
+                .get(&(r))
+                .expect("We should have the whole history by now")
+                .values()
+                .filter(|(digest, _)| parents.iter().any(|x| x.header.parents.contains(digest)))
                 .map(|(_, certificate)| certificate)
                 .collect();
         }
@@ -285,20 +258,15 @@ impl Consensus {
 
     /// Flatten the dag referenced by the input certificate. This is a classic depth-first search (pre-order):
     /// https://en.wikipedia.org/wiki/Tree_traversal#Pre-order
-    /// 把 DAG 展开成一个线性序列
     fn order_dag(&self, leader: &Certificate, state: &State) -> Vec<Certificate> {
         debug!("Processing sub-dag of {:?}", leader);
-        // 最终要返回的序列
         let mut ordered = Vec::new();
-        // 去重集合
         let mut already_ordered = HashSet::new();
-        // 用一个栈模拟 DFS，初始栈里只有 leader
+
         let mut buffer = vec![leader];
-        // 不断pop，处理节点
         while let Some(x) = buffer.pop() {
             debug!("Sequencing {:?}", x);
             ordered.push(x.clone());
-            // 遍历当前节点父引用
             for parent in &x.header.parents {
                 let (digest, certificate) = match state
                     .dag
